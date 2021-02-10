5ec1275343fd4155aa3226e7edcff036722642ac
Anton Fenske <anton@devskills.co>
2021-02-10 23:03:11 +0100

    Add working boilerplate.

---
+++ app/src/main/java/co/devskills/app/PingController.java
@@ -0,0 +1,16 @@
+package co.devskills.app;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping(value = "/ping")
+    @ResponseStatus(HttpStatus.OK)
+    public String healthCheck(){
+        return "pong";
+    }
+}

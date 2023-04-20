e455fbaafa86e4ec33f26a7e487d2040a963804b
gokulsinghkhetwal <38552752+gokulsinghkhetwal@users.noreply.github.com>
2023-04-20 16:40:46 +0200

    Update PingController.java

--- app/src/main/java/co/devskills/springbootboilerplate/PingController.java
+++ app/src/main/java/co/devskills/springbootboilerplate/PingController.java
@@ -1,5 +1,6 @@
 package co.devskills.springbootboilerplate;
 
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
